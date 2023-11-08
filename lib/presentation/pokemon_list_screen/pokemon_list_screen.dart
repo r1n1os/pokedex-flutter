ecbc686f0a4383ae7912cebcd201fb740a470eae
r1n1os <evagoras.frang@gmail.com>
2023-11-08 23:31:17 +0200

    load data on UI

--- lib/presentation/pokemon_list_screen/pokemon_list_screen.dart
+++ lib/presentation/pokemon_list_screen/pokemon_list_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokedex/data/local_database/entities/pokemon_entity.dart';
 import 'package:pokedex/presentation/pokemon_list_screen/pokemon_list_bloc/pokemon_list_bloc.dart';
 import 'package:pokedex/presentation/pokemon_list_screen/pokemon_list_bloc/pokemon_list_events.dart';
+import 'package:pokedex/presentation/pokemon_list_screen/pokemon_list_bloc/pokemon_list_states.dart';
 import 'package:pokedex/utils/get_it_initialization.dart';
 
 class PokemonListScreen extends StatefulWidget {
@@ -21,6 +24,53 @@ class _PokemonListScreenState extends State<PokemonListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: BlocProvider(
+        create: (providerContext) => _pokemonListBloc,
+        child: _buildView(),
+      ),
+    );
+  }
+
+  Widget _buildView() {
+    return BlocConsumer<PokemonListBloc, PokemonListStates>(
+      listener: (providerContext, state) {},
+      builder: (providerContext, state) {
+        return _buildViewBasedOnState(providerContext, state);
+      },
+    );
+  }
+
+  Widget _buildViewBasedOnState(
+      BuildContext providerContext, PokemonListStates state) {
+    if (state.error != null) {
+      return _buildErrorView(providerContext, state);
+    }
+    return _buildPokemonList(providerContext, state.pokemonEntityList ?? []);
+  }
+
+  Widget _buildErrorView(
+      BuildContext providerContext, PokemonListStates state) {
+    return Center(
+      child: Text(state.error ?? ''),
+    );
+  }
+
+  Widget _buildPokemonList(BuildContext providerContext, List<PokemonEntity> pokemonEntityList) {
+    return GridView.builder(
+        itemCount: pokemonEntityList.length,
+        itemBuilder: (context, index) {
+      return _pokemonCard(pokemonEntityList[index]);
+    }, gridDelegate:  const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      mainAxisSpacing: 8.0,
+      crossAxisSpacing: 8.0,
+    ),);
+  }
+
+  Widget _pokemonCard(PokemonEntity pokemonEntity) {
+    return Card(
+      child: Text(pokemonEntity.name ?? 'NA'),
+    );
   }
 }
